b074f3e305b690b109064c75b556cb05b08a3fa0
Dmytro <dimakoua@gmail.com>
2023-03-22 22:48:21 +0200

    adds Item2Header

---
+++ src/components/widgets/itemHeader2.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {  StyleSheet, View, Text,  TouchableOpacity } from 'react-native'
+
+export default function ItemHeader2({ data, navigation }) {
+    return (
+        <TouchableOpacity onPress={() => onPress(data.actions)}>
+            <View>
+                {data ?
+                    <>
+                        <Text style={[styles.title]}>{data.title}</Text>
+                        <Text style={[styles.sub_title]}>{data.sub_title}</Text>
+
+                        <View style={[styles.prices_list]}>
+                            <Text style={[styles.price_usd]}>USD {data.price.usd}</Text>
+                            <Text style={[styles.price]}>UAH {data.price.uah}</Text>
+                        </View>
+                    </>
+                    : null}
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 20
+    },
+    sub_title: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    prices_list: {
+        flex: 1,
+        flexDirection: 'row',
+        fontSize: 16
+    },
+    price_usd: {
+        color: 'green',
+        fontWeight: 'bold',
+        paddingRight: 10
+    }
+});
