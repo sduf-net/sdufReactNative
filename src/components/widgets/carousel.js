2b8f6c1793bf648be024eeb4c57437196135b36c
Dmytro <dimakoua@gmail.com>
2023-03-22 22:48:32 +0200

    adds carousel

--- src/components/widgets/carousel.js
+++ src/components/widgets/carousel.js
@@ -1,40 +1,66 @@
-// import React from 'react'
-// import { View, Text, Dimensions } from 'react-native';
-// import Carousel from 'react-native-reanimated-carousel';
-// // import 'react-native-reanimated';
+import * as React from 'react';
+import { Button, Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
 
-// const { width } = Dimensions.get('window');
+export default function CarouselReanimated({ data, navigation }) {
+    const width = Dimensions.get('window').width;
+    const ref = React.useRef(null);
 
-
-export default function CarouselReanimated() {
+    console.log("CarouselReanimated", data.images)
+    return (
+        <View style={{ flex: 1 }}>
+            <Carousel
+                loop
+                ref={ref}
+                width={width}
+                height={width / 2}
+                // autoPlay={true}
+                data={data.images}
+                // scrollAnimationDuration={5000}
+                onSnapToItem={(index) => console.log('current index:', index)}
+                renderItem={({ item }) => {
+                    console.log("OMAGE", item)
+                    return (
+                        <View
+                            style={{
+                                flex: 1,
+                                borderWidth: 1,
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <Image
+                                resizeMode={'cover'}
+                                style={[styles.image]}
+                                source={{ uri: item.src }}
+                            />
+                        </View>
+                    )
+                }}
+            />
+            <Button
+                title="Press Me"
+                onPress={() => {
+                    ref.current?.scrollTo({ count: -1, animated: true });
+                }}
+            >
+                prev
+            </Button>
+            <Button
+                title="Press Me"
+                onPress={() => {
+                    ref.current?.scrollTo({ count: 1, animated: true });
+                }}
+            >
+                next
+            </Button>
+        </View>
+    );
 }
 
-//     return (
-//         <View style={{ flex: 1 }}>
-//         <Carousel
-//             loop
-//             width={width}
-//             height={width / 2}
-//             autoPlay={true}
-//             data={[...new Array(6).keys()]}
-//             scrollAnimationDuration={1000}
-//             onSnapToItem={(index) => console.log('current index:', index)}
-//             renderItem={({ index }) => (
-//                 <View
-//                     style={{
-//                         flex: 1,
-//                         borderWidth: 1,
-//                         justifyContent: 'center',
-//                     }}
-//                 >
-//                     <Text style={{ textAlign: 'center', fontSize: 30 }}>
-//                         {index}
-//                     </Text>
-//                 </View>
-//             )}
-//         />
-//     </View>
-//     );
-// }
 
-
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 400
+    }
+});
