e26fad7feacf48cc3fcc7b079762000deaef1784
Dmytro <dimakoua@gmail.com>
2023-10-23 08:20:46 +0300

    update host

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,6 @@
-export const URL = "https://3348-38-49-174-212.ngrok-free.app";
-export const SOCKET_URL = "wss://3348-38-49-174-212.ngrok-free.app/socket";
+export const URL = "https://demo-sduf.gigalixirapp.com";
+export const SOCKET_URL = "wss://demo-sduf.gigalixirapp.com/socket";
 export const GET_SCREEN_BY_NAME = "request_screen";
 export const styleURL = 'https://api.maptiler.com/maps/streets/style.json?key=tnZNmNpDX9FyfLImwQud';
 export const INDEX_SCREEN = 'index';
 export const LOGIN_SCREEN = 'login';
-
