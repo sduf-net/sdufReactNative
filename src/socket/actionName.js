7791f691a8d460445924824b995f73d3de191c0d
Dmytro <dimakoua@gmail.com>
2024-02-11 21:51:17 +0200

    refactoring

--- src/socket/actionName.js
+++ src/socket/actionName.js
@@ -5,6 +5,6 @@ export const PAGINATION = "pagination";
 export const SUBMIT_FORM = "submit_form";
 export const REQUEST_WIDGET = "request_widget";
 
-export const ROUTE_TO_LOCAL = "routeToLocal";
-export const ROUTE_BACK = "routeBack";
-export const ROUTE_TO_SCREEN_FROM_API = "routeToScreenFromApi";
+export const ROUTE_TO_LOCAL = "route_to_local";
+export const ROUTE_BACK = "route_back";
+export const ROUTE_TO_SCREEN_FROM_API = "route_to_screen_from_api";
