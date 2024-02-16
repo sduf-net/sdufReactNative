d3dedadc470f4c31221c414576e93bd5ad437f80
Dmytro <dimakoua@gmail.com>
2024-02-16 05:44:42 +0200

    pass router params to server

--- src/components/widgets/api.js
+++ src/components/widgets/api.js
@@ -2,14 +2,19 @@ import React, { memo, useEffect } from 'react';
 import { View, ActivityIndicator } from 'react-native';
 import { useSelector } from 'react-redux';
 import { handleEventAction } from '../../event_handler';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 function ApiWidget({ data, id }) {
     const userId = useSelector(state => state.user.id);
     const navigation = useNavigation();
+    const route = useRoute();
 
     useEffect(() => {
-        handleEventAction({ id, type: "request_widget", callbackUrl: data.callbackUrl }, navigation);
+        handleEventAction({
+            id, type: "request_widget",
+            callbackUrl: data.callbackUrl,
+            params: data
+        }, navigation, route);
     }, [])
 
     return (
