16fc6b2a3230669754d522c18a6680b81983360d
Dmytro <dimakoua@gmail.com>
2024-05-16 06:58:47 +0300

    WIP get screen from memory

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,21 +4,24 @@ import { pushEventToChannel } from '../socket/socketAction';
 import WidgetList from '../components/widgetList';
 import FixedTop from '../components/fixedTop';
 import FixedBottom from '../components/fixedBottom';
-import { shallowEqual, useSelector } from 'react-redux';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import FloatingCard from '../components/layouts/floatingCard';
 import CustomModal from '../components/layouts/modalWindow';
 import { useNavigation, useFocusEffect, useRoute, useIsFocused } from '@react-navigation/native';
 import { getUserChannel } from '../socket/userChannel';
 import { GET_SCREEN_BY_NAME } from '../socket/actionName';
 import useErrors from '../hooks/useErrors';
+import { selectCurrentScreenByName, setCurrentScreenId } from '../redux/screens';
 
 const INDEX_SCREEN = 'index';
 
 export default function IndexScreen() {
     const isFocused = useIsFocused();
+    const dispatch = useDispatch();
     const navigation = useNavigation();
     const route = useRoute();
     const userId = useSelector(state => state.user.id, shallowEqual);
+    const screensState = useSelector(state => state.screens, shallowEqual);
     // const { newError } = useErrors()
 
     const [loading, setLoading] = useState(false);
@@ -53,14 +56,22 @@ export default function IndexScreen() {
         const queryString = route?.params || null;
         const screenName = route?.params?.screenName || INDEX_SCREEN;
 
-        pushEventToChannel(getUserChannel(), {
-            userId: userId,
-            actionName: GET_SCREEN_BY_NAME,
-            payload: {
-                query: queryString,
-                screen_name: screenName
-            }
-        })
+        // TODO refactor
+        // onreload
+        const screen = selectCurrentScreenByName(screensState, screenName);
+        if (screen.length) {
+            console.log("jkjkjkjkjkj",screen[0].id)
+            dispatch(setCurrentScreenId(screen[0].id));
+        } else {
+            pushEventToChannel(getUserChannel(), {
+                userId: userId,
+                actionName: GET_SCREEN_BY_NAME,
+                payload: {
+                    query: queryString,
+                    screen_name: screenName
+                }
+            })
+        }
 
         setTimeout(() => {
             setLoading(false);
