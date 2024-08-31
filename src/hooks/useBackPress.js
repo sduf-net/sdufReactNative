1cc9f4eb2012ea918b18a08e0ddce8cc2c0908d4
Dmytro Kozin <dimakoua@gmail.com>
2024-08-31 11:26:15 -0400

    add back press hook

---
+++ src/hooks/useBackPress.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react';
+import { BackHandler } from 'react-native';
+
+const useBackPress = (onBackPress, debounceTime = 500) => {
+  const lastPressTime = useRef(0);
+
+  useEffect(() => {
+    const handleBackPress = () => {
+      const currentTime = new Date().getTime();
+      if (currentTime - lastPressTime.current > debounceTime) {
+        lastPressTime.current = currentTime;
+        if (onBackPress) {
+          onBackPress();
+        } else {
+          BackHandler.exitApp();
+        }
+      }
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+    return () => backHandler.remove();
+  }, [onBackPress, debounceTime]);
+};
+
+export default useBackPress;
