3db655a5dd5944c4515605d8aa38e739ee9501a0
Dmytro Kozin <dimakoua@gmail.com>
2024-09-05 19:52:15 -0400

    persist screens

---
+++ src/redux/persistor.js
@@ -0,0 +1,43 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { isPermanent } from "../utils/cache";
+import { setCurrentUser } from "./users";
+import { setScreen } from "./screens";
+import { isEmpty } from "../components/helpers/utils";
+import store from "./store";
+
+export const Persistor = {
+    persist: async function (key, value) {
+        const stringify = JSON.stringify(value);
+        await AsyncStorage.setItem(key, stringify);
+    },
+    fetch: async function (key) {
+        return await AsyncStorage.getItem(key);
+    },
+    remove: async function (key) {
+        await AsyncStorage.removeItem(key);
+    },
+    restoreUser: async function () {
+        const user = await AsyncStorage.getItem(this.USER);
+        if (user) {
+            store.dispatch(setCurrentUser(JSON.parse(user)));
+        }
+    },
+    restoreScreens: async function () {
+        const screens = await AsyncStorage.getItem(this.SCREENS);
+        const screensObj = JSON.parse(screens);
+
+        if (!isEmpty(screensObj)) {
+            Object.keys(screensObj).forEach(id => {
+                if (isPermanent(screensObj[id])) {
+                    store.dispatch(setScreen(screensObj[id]));
+                }
+            });
+        }
+    },
+    restoreStore: async function () {
+        await this.restoreUser();
+        await this.restoreScreens();
+    },
+    'USER': 'user',
+    'SCREENS': 'screens'
+}
--- src/utils/cache.js
+++ src/utils/cache.js
@@ -4,11 +4,17 @@ export const TTL = 'ttl';
 export const NONE = 'none';
 
 // rn permanent is not suported and work as session
+export const isPermanent = (screen) => {
+  if (screen?.config?.cache_strategy === PERMANENT) return true;
+  return false;
+}
+
 export const isLoadFromCache = (screen) => {
   if (!screen?.config?.cache_strategy) return false;
 
   let isLoadFromCache = false;
   switch (screen.config.cache_strategy) {
+    case PERMANENT:
     case PER_SESSION:
       isLoadFromCache = true;
       break;
