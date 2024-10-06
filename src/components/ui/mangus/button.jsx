1c5f71d48a6d659eb729c257188bd373dca3884c
Dmytro Kozin <dimakoua@gmail.com>
2024-10-06 13:35:24 -0400

    handle actions in mungus btn

--- src/components/ui/mangus/button.jsx
+++ src/components/ui/mangus/button.jsx
@@ -1,17 +1,31 @@
 
 import * as React from "react";
 import { VirtualizedList } from "react-native";
-import { Button, Icon, Text } from "react-native-magnus";
+import { Button, Icon } from "react-native-magnus";
 import { getItem, getItemCount } from "../../../utils";
+import { onLongPress, onPress } from "../../../event_handler";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 const ButtonWidget = (config) => {
     const { data } = config;
+    const route = useRoute();
+    const navigation = useNavigation();
     const renderWidget = ({ item }) => <config.factory props={item} />;
+
+    const onPressHandle = () => {
+        onPress(data.actions, navigation, route);
+    };
+
+    const onLongPressHandle = () => {
+        onLongPress(data.actions, navigation, route);
+    };
 
     return (
         <>
             {data.props && <Button
                 {...data.props}
+                onPress={onPressHandle}
+                onLongPress={onLongPressHandle}
                 prefix={data.prefix ? <Icon {...data.prefix.props} /> : null}
                 suffix={data.suffix ? <Icon {...data.suffix.props} /> : null}
             >
